9d464484cde0d287e1acbd60e53c876ea1e731da
Дмитрий Антонов <dmitrijantonov@MacBook-Air-Dmitrij.local>
2024-12-27 17:50:34 +0300

    first commit

---
+++ 1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    /*
+    double n, k, count = 0, sum = 0;
+    cin >> n >> k;
+    vector<double> numbers(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> numbers[i];
+    }
+    
+    for (int i = 0; i < n; ++i)
+    {
+        if (numbers[i] == k)
+        {
+            ++count;
+            continue;
+        }
+        sum = numbers[i];
+        for (int j = (i + 1); j < n; ++j)
+        {
+            sum += numbers[j];
+            if (sum == k)
+            {
+                ++count;
+                int size = n + 1;
+                j = size;
+                continue;
+            }
+        }
+    }
+    cout << count << endl;*/
+    for ( int i  = 0 ; i < 100000; ++i )
+    {
+        cout << 100000000 << endl;
+    }
+    return 0;
+}
